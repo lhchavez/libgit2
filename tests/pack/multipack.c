917bda625821f70b0d62cd78beca92be157088d6
lhchavez <lhchavez@lhchavez.com>
2020-02-23 22:28:52 +0000

    multipack: Introduce a parser for multi-pack-index files
    This change is the first in a series to add support for git's
    multi-pack-index. This should speed up large repositories significantly.
    
    Part of: #5399

---
+++ tests/pack/multipack.c
@@ -0,0 +1,29 @@
+#include "clar_libgit2.h"
+
+#include <git2.h>
+
+#include "multipack.h"
+
+void test_pack_multipack__parse(void)
+{
+	git_repository *repo;
+	struct git_multipack_index_file *idx;
+	struct git_multipack_entry e;
+	git_oid id;
+	char midx_path[1025];
+
+	cl_git_pass(git_repository_open(&repo, cl_fixture("testrepo.git")));
+	p_snprintf(
+			midx_path, sizeof(midx_path), "%s/objects/pack/multi-pack-index", git_repository_path(repo));
+	cl_git_pass(git_multipack_index_open(&idx, midx_path));
+
+	cl_git_pass(git_oid_fromstr(&id, "5001298e0c09ad9c34e4249bc5801c75e9754fa5"));
+	cl_git_pass(git_multipack_index_entry_find(&e, idx, &id, GIT_OID_HEXSZ));
+	cl_assert_equal_oid(&e.sha1, &id);
+	cl_assert_equal_s(
+			(const char *)git_vector_get(&idx->packfile_names, e.pack_index),
+			"pack-d7c6adf9f61318f041845b01440d09aa7a91e1b5.idx");
+
+	git_multipack_index_free(idx);
+	git_repository_free(repo);
+}
